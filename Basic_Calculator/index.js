d682a097a6ceca05ea6c6c06eced8f0831258e07
Niloy Sarker Kakon <kakonsorkar1@gmail.com>
2026-05-27 20:07:51 +0600

    .

---
+++ Basic_Calculator/index.js
@@ -0,0 +1,28 @@
+function sumAdd(a , b){
+    result = a + b;
+    console.log("Addition of a and b is: " + result);
+}
+
+function sumSub(a , b){
+    result = a - b;
+    console.log("Subtraction of a and b is: " + result);
+}
+
+function sumMul(a , b){
+    result = a * b;
+    console.log("Multiplication of a and b is: " + result);
+}
+
+function sumDiv(a , b){
+    result = a / b;
+    console.log("Division of a and b is: " + result);
+}
+
+
+// call the function for result 
+//eg.
+
+sumAdd( 4, 6);
+sumSub( 4, 6);
+sumMul( 4, 6);
+sumDiv( 4, 6);
