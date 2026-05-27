2ef2a5c2bf220b8f8432c510c750f2c7495252bd
Niloy Sarker Kakon <kakonsorkar1@gmail.com>
2026-05-27 19:42:32 +0600

    .

---
+++ Functions/index.js
@@ -0,0 +1,21 @@
+//Create a function using the "function" keyword that takes a string as an argument and returns the number of vowels in the string.
+
+
+function countVowels(str){
+    let count = 0;
+    for(const char of str){
+        if(char ==="a" || 
+           char ==="e" || 
+           char ==="i" || 
+           char ==="o" || 
+           char ==="u"
+           ){
+            count ++;
+        }
+    }
+
+    console.log("Number of Vowels in the String: " + count);
+}
+
+//call the function for result
+// countVowels("Cat");
