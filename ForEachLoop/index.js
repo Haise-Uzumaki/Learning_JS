6c5dcf098c04fc103c1afad29976af12b0d7ff4f
Niloy Sarker Kakon <kakonsorkar1@gmail.com>
2026-05-27 20:03:18 +0600

    .

---
+++ ForEachLoop/index.js
@@ -0,0 +1,8 @@
+// For a given array of numbers, print the square of each value using the forEach Loop
+
+
+let num = [2, 3, 6, 68, 23, 45];
+
+num.forEach((num) => {
+    console.log(num * num);
+});
